23d71db15bc654271b969b1393214ef85196feea
AR-Fahad <ashfaqurrahmanfahad@gmail.com>
2023-11-09 19:01:40 +0600

    server side main link added

--- src/AxiosInstance/instance.js
+++ src/AxiosInstance/instance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   withCredentials: true,
-  baseURL: "http://localhost:5000",
+  baseURL: "https://career-maker-server-steel.vercel.app",
   headers: {
     "Content-Type": "application/json",
   },
